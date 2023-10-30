1f07780520bbf31bd9c75a8a59db291c165c8dc1
William Inyam <sw.inyam@outlook.com>
2023-10-30 23:43:32 +0000

    8-concat_edges.py update 1

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -2,4 +2,4 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 print(f"{str[39:66]}", end=" ")
-print(f"{str[107:111]} {str[0:7]}")
+print(f"{str[107:111]} {str[0:6]}")
