0cab83fc500207727716fd458f0b5b4961b3db26
William Inyam <sw.inyam@outlook.com>
2023-10-31 01:25:09 +0000

    10-check_cycle.c update 2

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,7 +9,7 @@ int check_cycle(listint_t *list)
 {
 	listint_t *slow, *fast;
 
-	if (list == NULL)
+	if (list == NULL || list->next == NULL)
 		return (0);
 	slow = fast = list;
 	while ((fast = fast->next->next))
