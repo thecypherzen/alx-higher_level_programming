327a0b3a1de80dda5c9cc066992e7bd37ad20e55
William Inyam <sw.inyam@outlook.com>
2023-10-31 10:43:05 +0000

    1-last_digit.py submission

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+ld = number % 10
+
+
+# function to create response message
+def getmsg(num):
+    if num > 5:
+        return "greater than 5"
+    elif num < 6 and num != 0:
+        return "less than 6 and not 0"
+    else:
+        return "0"
+
+
+print(f"Last digit of {number} is {ld} and is {getmsg(ld)}")
