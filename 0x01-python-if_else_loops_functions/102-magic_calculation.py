b74202fa388e1531021be31b69725c84b6ddd56a
William Inyam <sw.inyam@outlook.com>
2023-10-31 23:01:16 +0000

    102-magic_calculation.py submission

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+# Python function from bytecode
+def magic_calculation(a, b, c):
+    if (a < b):
+        return c
+    elif (c > b):
+        return a + b
+    return a*b - c
