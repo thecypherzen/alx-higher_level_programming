db092d5d0cb8e0aae0a936124380405332365861
William Inyam <sw.inyam@outlook.com>
2023-10-31 13:39:20 +0000

    11-pow.py submission

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+# A function that computes a to the power of b and return the value.
+# Returns the value of a ^ b
+# No module import allowed
+"""
+
+
+def pow(a, b):
+    return a ** b
