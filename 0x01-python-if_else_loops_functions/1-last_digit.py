5778823297ceaed3c385c1390181e72d4b3709be
William Inyam <sw.inyam@outlook.com>
2023-10-31 11:32:56 +0000

    1-last_digit.py update 2

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-ld = number % 10 if number > 0 else (number * -1) % 10
+ld = number % 10 if number > 0 else ((number * -1) % 10) * -1
 
 
 # function to create response message
