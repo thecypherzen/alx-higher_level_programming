440be8eb3749a5214a7bff6ac27c6ff3e7acd6d9
William Inyam <sw.inyam@outlook.com>
2023-10-31 13:35:09 +0000

    10-add.py submission

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+# A function that adds two integers and returns the result.
+# Returns the value of a + b
+# Module Import Not allowed
+"""
+
+
+def add(a, b):
+    return a + (b)
