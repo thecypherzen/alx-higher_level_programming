bc7f405e4cf93d00ffca29614f0e93ea5e602318
William Inyam <sw.inyam@outlook.com>
2023-11-02 21:50:27 +0000

    2-args.py submission

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Prints the number of and list of command line arguments
+
+    Args:
+        Nill
+
+    Return:
+        None
+    """
+    import sys
+    args = sys.argv
+    len = len(args)
+
+    if len == 1:
+        print("0 arguments.")
+    else:
+        a_msg = "arguments" if len > 2 else "argument"
+        print("{} {}:".format(len - 1, a_msg))
+        for i in range(1, len):
+            print("{}: {}".format(i, args[i]))
