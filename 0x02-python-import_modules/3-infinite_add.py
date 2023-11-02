06e18fb7e7f494be164aaaf3d20abf12af38fef5
William Inyam <sw.inyam@outlook.com>
2023-11-02 22:32:31 +0000

    3-infinite_add.py  submission

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    """Prints the result of the addition of all commandline
+    arguments.
+
+    Args:
+        Nill
+
+    Return:
+        None
+    """
+    import sys
+    from calculator_1 import add
+    args_list = sys.argv
+    args_len = len(args_list)
+
+    sum = 0
+    if args_len == 1:
+        print("0")
+    else:
+        for i in range(1, args_len):
+            sum = (add(sum, int(args_list[i])))
+        print(sum)
