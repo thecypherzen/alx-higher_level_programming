ff0989e317693c7cb26382c02157bfab6521804f
William Inyam <sw.inyam@outlook.com>
2023-11-03 09:25:46 +0000

    5-variable_load.py submission

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """Imports a variable from a module and prints it.
+        simple enough.
+    """
+    from variable_load_5 import a
+    print(a)
