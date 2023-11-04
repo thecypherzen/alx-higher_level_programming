a9c0f313b7343f25b7323d4025496aac9cbce922
William Inyam <sw.inyam@outlook.com>
2023-11-04 15:00:59 +0000

    3-print_reversed_list_integer.py submission

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def print_reversed_list_integer(my_list=[]):
+    """Prints integer elements of a list in reversed order.
+
+    Description:
+        - Prints one integer per line
+        - Cannot cast integers to strings
+        - Try/Except is not allowed
+        - Module Imports not allowed
+        - Must use str.format() to print
+
+    Return:
+        None
+    """
+    max = len(my_list)
+    for i in range(max - 1, -1, -1):
+        print("{:d}".format(my_list[i]))
---
+++ 0x03-python-data_structures/tests/3-main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
+
+my_list = [32, -3030, 1, 2, 3, 4, 5, 19, 20]
+print_reversed_list_integer(my_list)
