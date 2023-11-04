505f80fc96a7ca21eb307fb9e84cf29acfbd67ad
William Inyam <sw.inyam@outlook.com>
2023-11-04 15:04:10 +0000

    Task 3 Update 1 - handling none input

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -13,6 +13,8 @@ def print_reversed_list_integer(my_list=[]):
     Return:
         None
     """
-    max = len(my_list)
-    for i in range(max - 1, -1, -1):
+    if my_list is None:
+        return None
+    max = len(my_list) - 1
+    for i in range(max, -1, -1):
         print("{:d}".format(my_list[i]))
