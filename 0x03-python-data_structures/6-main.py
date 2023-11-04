652f2b66506da277da52acadc3da6f725ee06b0b
William Inyam <sw.inyam@outlook.com>
2023-11-04 16:40:14 +0000

    6-print_matrix_integer.py update 1 - docstrings

--- 0x03-python-data_structures/6-main.py
+++ 0x03-python-data_structures/6-main.py
@@ -2,11 +2,11 @@
 print_matrix_integer = __import__('6-print_matrix_integer').print_matrix_integer
 
 matrix = [
-    [1, 2, 3],
+    None,
     [4, 5, 6],
     [7, 8, 9]
 ]
 
-print_matrix_integer(None)
+print_matrix_integer(matrix)
 print("--")
 print_matrix_integer()
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    """Prints a matrix of integers.
 
+    Details:
+        - Cannot cast integers to strings
+        - must use str.format() to print
+        - Module imports not allowed
+
+    parameters:
+        @matrix: the matrix
+
+    Return:
+        None
+    """
     if matrix is not None:
         for row in matrix:
             if row is not None:
