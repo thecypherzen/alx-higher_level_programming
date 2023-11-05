fcbac989d9b7591e9bcbf032454f550c15b469b5
William Inyam <sw.inyam@outlook.com>
2023-11-05 01:32:23 +0000

    7-add_tuple.py update 1

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -13,18 +13,19 @@ def add_tuple(tuple_a=(), tuple_b=()):
 
     """
     if tuple_a is None and tuple_b is not None:
-        return tuple_b
+        return tuple(tuple_b[i] for i in range(len(tuple_b)) if i < 2)
     if tuple_a is not None and tuple_b is None:
-        return tuple_a
+        return tuple(tuple_a[i] for i in range(len(tuple_a)) if i < 2)
     if tuple_a is None and tuple_b is None:
         return None
-    len_a = len(tuple_a)
-    len_b = len(tuple_b)
-
-    first = tuple_a if len_a > len_b else tuple_b
-    second = tuple_a if len_a < len_b else tuple_b
-
-    ret = [i for i in first]
-    for i in range(len(second)):
-        ret[i] += second[i]
-    return tuple(ret)
+    res = []
+    for i, j in enumerate(tuple_a):
+        if i < 2:
+            res.append(j)
+    for i, j in enumerate(tuple_b):
+        if i < 2:
+            if i == len(res):
+                res.append(j)
+            else:
+                res[i] += j
+    return tuple(res)
