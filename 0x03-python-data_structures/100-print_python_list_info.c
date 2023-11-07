338b7811eeed396b7fa6f214ee851ae0d7ec2b7c
William Inyam <sw.inyam@outlook.com>
2023-11-07 02:51:11 +0000

    Update 100-print_python_list.info.c for betty

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
-#include "Python.h"
 #include <time.h>
+#include "Python.h"
 
+/**
+ * print_python_list_info - prints info of a python list
+ * @p: pointer to python list object
+ * Return: void
+ */
 void print_python_list_info(PyObject *p)
 {
 	PyListObject *list = (PyListObject *)p;
+
 	printf("[*] Size of the Python List = %ld\n", list->ob_base.ob_size);
 	printf("[*] Allocated = %ld\n", list->allocated);
 }
