c1a1d9ca71661f3ec016332fab40d40f18d33af4
William Inyam <sw.inyam@outlook.com>
2023-11-07 14:40:55 +0000

    Task 14 Resolved

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,6 +1,10 @@
-#include <stdio.h>
-#include <time.h>
 #include "Python.h"
+#include <stdio.h>
+#include <sys/types.h>
+#include <listobject.h>
+/* #define _XOPEN_SOURCE 700 */
+
+
 
 /**
  * print_python_list_info - prints info of a python list
@@ -10,7 +14,13 @@
 void print_python_list_info(PyObject *p)
 {
 	PyListObject *list = (PyListObject *)p;
+	Py_ssize_t size = list->ob_base.ob_size, i;
 
 	printf("[*] Size of the Python List = %ld\n", list->ob_base.ob_size);
 	printf("[*] Allocated = %ld\n", list->allocated);
+	printf("Elements: %ld\n", size);
+	for (i = 0; i < size; i++)
+	{
+		printf("Element %ld: %s\n", i, list->ob_item[i]->ob_type->tp_name);
+	}
 }
