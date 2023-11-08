0c7a5fdd09b620e7731a3eef0c2b9dd03a3b3938
William Inyam <sw.inyam@outlook.com>
2023-11-08 19:08:33 +0000

    Task 4-only_diff_elements.py submission

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    """A function that returns a set of all elements present in
+        only one set.
+
+    Parameters:
+        @set_1: the first set
+        @set_2: the second set
+
+    Return: a set of exclusives
+
+    Notes:
+        - No imports allowed
+    """
+    if not set_1 or not set_2:
+        return set()
+    return set((set_1 | set_2) - (set_1 & set_2))
