bb844be7cedb8c79e8744ee6b636b2e021d6342b
William Inyam <sw.inyam@outlook.com>
2023-11-08 20:07:06 +0000

    7-update_dictionary.py submission

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    """A function that replaces or adds key/value in a dictionary.
+
+    Params:
+        @a_dictionary: the dictionary
+        @key: key of new value - of type str
+        @value: new value - of any type
+
+    Return:
+        The dictionary with updated values
+    """
+    if not a_dictionary:
+        return a_dictionary
+    a_dictionary[key] = value
+    return a_dictionary
