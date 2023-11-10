5d0db122443f2f36260d2c4034559f02b07d989f
William Inyam <sw.inyam@outlook.com>
2023-11-10 17:15:23 +0000

    Task 16 Update 3

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -15,13 +15,13 @@ void print_python_bytes(PyObject *p)
 	PyBytesObject *obj;
 	Py_ssize_t objsize, max_len, i;
 
+	printf("[.] bytes object info\n");
 	if (!is_bytes)
-		printf("[ERROR] Invalid Bytes Object\n");
+		printf("  [ERROR] Invalid Bytes Object\n");
 	else
 	{
 		obj = (PyBytesObject *)p;
 		objsize = obj->ob_base.ob_size;
-		printf("[.] bytes object info\n");
 		printf("  size: %ld\n", objsize);
 		printf("  trying string: %s\n", obj->ob_sval);
 		max_len = objsize + 1 <= 10 ? objsize + 1 : 10;
@@ -42,10 +42,16 @@ void print_python_list(PyObject *p)
 {
 	PyListObject *listobj = (PyListObject *)p;
 	Py_ssize_t i = 0, list_sz = listobj->ob_base.ob_size;
+	const char *name;
 
 	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %ld\n", list_sz);
-	printf("[*] Allocated: %ld\n", listobj->allocated);
+	printf("[*] Allocated = %ld\n", listobj->allocated);
 	for (i = 0; i < list_sz; i++)
-		printf("Element: %ld, %s\n", i, listobj->ob_item[i]->ob_type->tp_name);
+	{
+		name = listobj->ob_item[i]->ob_type->tp_name;
+		printf("Element %ld: %s\n", i, listobj->ob_item[i]->ob_type->tp_name);
+		if (!strcmp(name, "bytes"))
+			print_python_bytes(listobj->ob_item[i]);
+	}
 }
---
+++ 0x04-python-more_data_structures/main_9.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import ctypes
+
+lib = ctypes.CDLL('./libPython.so')
+lib.print_python_list.argtypes = [ctypes.py_object]
+lib.print_python_bytes.argtypes = [ctypes.py_object]
+l = ["Holberton"]
+lib.print_python_list(l)
+lib.print_python_bytes(l)
