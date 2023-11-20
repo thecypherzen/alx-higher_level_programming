6fdf7304ee7201f9dcaec83a1e177f6be9698c08
William Inyam <sw.inyam@outlook.com>
2023-11-20 12:36:59 +0000

    Task 0 update 1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -24,5 +24,16 @@ def safe_print_list(my_list=[], x=0):
                 print("")
             count += 1
     except Exception:
-        pass
+        print("")
     return count
+
+
+
+my_list = [1, 2, 3, 4, 5]
+
+nb_print = safe_print_list(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
