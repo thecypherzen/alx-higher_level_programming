52dbc95d75d58dd801f31c562ca48b72b0a8a436
William Inyam <sw.inyam@outlook.com>
2023-11-20 12:33:47 +0000

    Task 0 submission

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""A function that prints x elements of a list.
+
+Parameters:
+    @my_list: the list
+    @x: number of elements to print
+Return:
+    - the actual number of list elements printed
+Description:
+    - All elements must be printed on the same line followed by a
+      new line.
+    - x can be bigger than the length of my_list
+    - Not allowed to import any module
+    - Not allowed to use len()
+"""
+
+
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    try:
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+            if i == x - 1:
+                print("")
+            count += 1
+    except Exception:
+        pass
+    return count
