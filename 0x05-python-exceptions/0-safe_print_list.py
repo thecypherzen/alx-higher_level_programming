0c920b32375123ef2a0f0af0d9cb6be8d0d5e13a
William Inyam <sw.inyam@outlook.com>
2023-11-21 01:02:28 +0000

    Task 0 update 4

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -21,8 +21,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print("{}".format(my_list[i]), end="")
             count += 1
-    except IndexError:
-        print("Index Error")
-    if count > 0:
-        print("")
+    except Exception:
+        pass
+    print("")
     return count
