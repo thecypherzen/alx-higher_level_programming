58ffe4bf1632db9d1cc0ad20145b1acb33881296
William Inyam <sw.inyam@outlook.com>
2023-11-21 20:36:40 +0000

    Task 1 update 2

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 class Square():
-    """A class that defines a square"""
+    """A class that defines a square.
+
+    Attriubutes:
+        __size(:obj: `int`): size of the square's side
+
+    """
 
     def __init__(self, size):
-        """Square class initialization
-
-        Args:
-            __size(:obj: `int`): size of the square's side
-        """
+        """Square class initialization"""
         self.__size = size
