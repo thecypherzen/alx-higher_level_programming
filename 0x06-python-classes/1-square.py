8e9c43f09f953fe8735211e2130d04e48dffe520
William Inyam <sw.inyam@outlook.com>
2023-11-21 20:19:12 +0000

    Task 1 submission

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class Square():
+    """A class that defines a square
+
+    Attributes:
+        size (:obj:`int` or :`float`): size of the square in given units
+    """
+    def __init__(self, size):
+        self.__size = size
