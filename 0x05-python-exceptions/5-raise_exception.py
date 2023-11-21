dac4e9ee19182926e78d70232dd441ca30eaf045
William Inyam <sw.inyam@outlook.com>
2023-11-21 02:44:33 +0000

    Task 5 submission

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def raise_exception():
+    """A function that raises a type exception.
+
+    Parameters:
+        None
+
+    Return:
+        None
+
+    Description:
+        - Not allowed to import any module
+    """
+
+    raise TypeError
