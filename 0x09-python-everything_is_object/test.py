f7dfd1af99b6017feea0c5031e97574817c78daa
William Inyam <sw.inyam@outlook.com>
2023-11-28 16:33:04 +0000

    Tasks 20-28 submission

--- 0x09-python-everything_is_object/test.py
+++ 0x09-python-everything_is_object/test.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
-def increase(a, b):
-	b = a
+#def increase(a, b):
+#	b = a
 
-a = [3, 9]
-b = [1, 2, 4, 7]
-increase(a, b)
-print(a)
+#a = [3, 9]
+#b = [1, 2, 4, 7]
+#increase(a, b)
+#print(a)
+
+a = (1)
+b = (1)
+a = (1, 2)
+b = (1, 2)
+
+a = ()
+b = ()
+
+a = [1, 2, 3, 4]
+print(id(a))
+a += [5]
+print(id(a))
