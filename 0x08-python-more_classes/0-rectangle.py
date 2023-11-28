52119a50ca596b7711b3c02d61d02be7ccbe7d6c
William Inyam <sw.inyam@outlook.com>
2023-11-28 01:31:04 +0000

    Task 0 submission

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""A Module for More Classes and Objects ALX Project"""
+
+
+class Rectangle:
+    """A Class that defines a rectangle
+
+    Attributes:
+        None for now
+    """
+    pass
