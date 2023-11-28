524b22f53611d782992ace45ce3a6e82d6cb2191
William Inyam <sw.inyam@outlook.com>
2023-11-28 16:35:26 +0000

    Task 25 update 1

--- 0x09-python-everything_is_object/test.py
+++ 0x09-python-everything_is_object/test.py
@@ -7,15 +7,15 @@
 #increase(a, b)
 #print(a)
 
-a = (1)
-b = (1)
+#a = (1)
+#b = (1)
 a = (1, 2)
 b = (1, 2)
+print(a is b)
+#a = ()
+#b = ()
 
-a = ()
-b = ()
-
-a = [1, 2, 3, 4]
-print(id(a))
-a += [5]
-print(id(a))
+#a = [1, 2, 3, 4]
+#print(id(a))
+#a += [5]
+#print(id(a))
