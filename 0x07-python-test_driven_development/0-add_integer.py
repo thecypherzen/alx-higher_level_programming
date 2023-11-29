a001d94aead44fc53a0892b438190227c5dfac96
William Inyam <sw.inyam@outlook.com>
2023-11-29 08:38:09 +0000

    Task 0 update 2

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -16,9 +16,11 @@ def add_integer(a, b=98):
         sum of a and b
         always returns type int
     """
-    if any(i is None for i in (a, b)) \
-        or any(not isinstance(i, int) and not isinstance(i, float)
-               for i in (a, b)):
+    if a is None or (not isinstance(a, int)
+					and not isinstance(a, float)):
         raise TypeError("a must be an integer or b must be an integer")
+    if b is None or (not isinstance(b, int)
+					and not isinstance(b, float)):
+        raise TypeError("b must be an integer or b must be an integer")
     res = int(a) + int(b)
     return res
