942fb02d10aee4e80c06574b2623ae59b3ef2cb8
William Inyam <sw.inyam@outlook.com>
2023-11-29 15:20:38 +0000

    Task 2 update 2

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -24,7 +24,8 @@ def say_my_name(first_name, last_name=""):
             message:
                 ``first_name must be a string or last_name must be a string``
     """
-    if any(not isinstance(arg, str) for arg in (first_name, last_name)):
-        raise TypeError("first_name must be a string or " +
-                        "last_name must be a string")
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
