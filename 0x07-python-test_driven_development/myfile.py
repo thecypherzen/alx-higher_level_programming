05c16cc050be29dbcdd14b5dfb9c302e1a438f76
William Inyam <sw.inyam@outlook.com>
2023-11-29 17:48:46 +0000

    Task 3 submission

--- 0x07-python-test_driven_development/myfile.py
+++ 0x07-python-test_driven_development/myfile.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python3
-print(round(5.2/-3, 2))
-print(round(2/-3, 2))
-print(round(-8/-3, 2))
-print(round(-20/-3, 2))
+
+import sys
+from io import StringIO
+
+wstream = open("iostream", "w")
+rstream = open("iostream", "r")
+
+sys.stdout = wstream
+print("Line1\nLine2\nLine3\n", end="")
+wstream.truncate(0)
+wstream.seek(0)
+sys.stdout = sys.__stdout__
+
+#if rstream.tell():
+#    rstream.seek(0)
+wstream.write("newline\n")
+wstream.seek(0)
+
+lines = rstream.readlines()
+print(lines)
