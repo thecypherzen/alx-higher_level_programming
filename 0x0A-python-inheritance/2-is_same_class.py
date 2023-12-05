6d0725e69eb01fccc36cfccdce19028f08f718e6
William Inyam <sw.inyam@outlook.com>
2023-12-05 04:08:57 +0000

    Task 3 submission

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -2,12 +2,15 @@
 """0x0A. Python - Inheritance module - Task 2
 
 Functions:
-    is_same_class: inherits from ``list`` object
+    is_same_class: checks if an obj is an exact instance of another
 """
 
 
 def is_same_class(obj, a_class):
     """Tests if an object is an instance of a class
+
+    Checks if `obj` is and instance of `a_class` but not any of
+        ``a_class``'s base classes.
 
     Parameters:
         obj(:obj): some object
