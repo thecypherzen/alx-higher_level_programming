069a18dde5e83d300cb2c4345adadf63fb7ce514
William Inyam <sw.inyam@outlook.com>
2023-12-05 07:58:51 +0000

    Task 7 update 1

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,35 @@
+
+#!/usr/bin/python3
+"""0x0A. Python - Inheritance module - Task 7
+
+Classes:
+    BaseGeometry:
+"""
+
+
+class BaseGeometry:
+    """An  geometry base class
+
+    Attributes:
+        area(method): raises exception
+        integer_validator(method): validates value
+    """
+    def area(self):
+        """Raises an exception"""
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates class values
+
+        Raises:
+            TypeError: if ``value`` is not an integer.
+                Message: ``<name> must be an integer``
+            ValueError: if value is less or equal to 0:
+                Message: ``<name> must be greater than 0``
+        """
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+        pass
