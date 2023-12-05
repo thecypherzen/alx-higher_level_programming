bd9e849575b7e856a6fcf4728c0f0d24cd477846
William Inyam <sw.inyam@outlook.com>
2023-12-05 04:25:53 +0000

    Task 4 submission

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -2,14 +2,14 @@
 """0x0A. Python - Inheritance module - Task 3
 
 Functions:
-    is_kind_of_class: inherits from ``list`` object
+    is_kind_of_class: checks an obj is an instance of a class
 """
 
 
 def is_kind_of_class(obj, a_class):
     """Tests if an object is an instance of a class
 
-    Checks if `obj` is and instance of `a_class` or any of
+    Checks if `obj` is an instance of `a_class` or any of
         ``a_class``'s base classes.
 
     Parameters:
