3c3de951c95273298ae696970f379eae53350c42
William Inyam <sw.inyam@outlook.com>
2023-12-05 04:28:32 +0000

    Task 5 submission

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""0x0A. Python - Inheritance module - Task 4
+
+Functions:
+    inherits_from: checks if an object is a child of a class
+"""
+
+
+class BaseGeometry:
+    """An empty geometry base class"""
+    pass
