2fd43a9481a597a92063e8e5e1198d4cda6c04c0
William Inyam <sw.inyam@outlook.com>
2023-12-05 09:31:57 +0000

    Task 9 submission

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""0x0A. Python - Inheritance module - Task 8
+
+Classes:
+    Rectangle: BaseGeometry child.
+"""
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Inherits from BaseGeometry
+
+    Attributes:
+        __init__(method): class initializer
+    """
+    def __init__(self, width, height):
+        """Initialises a Rectangle
+
+        Params:
+            width: rectangle width
+            height: rectangle height
+
+        Returns:
+            None
+
+        Raises: Errors raised by BaseGeometry.integer_validator()
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Calculates area of rectangle
+
+        Returns: area of rectangle
+        """
+        return self.__width * self.__height
+
+    def __repr__(self):
+        """Representation of rectangle
+
+        Returns: ``[Rectangle] <width>/<height>``
+        """
+        return f"[Rectangle] {self.__width}/{self.__height}"
