aeaa6accd80264ea5d6282c990673dc301f485ba
William Inyam <sw.inyam@outlook.com>
2023-12-06 11:20:42 +0000

    Task 7 update 1

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -24,4 +24,4 @@ def save_to_json_file(my_obj, filename):
     """
     import json
     with open(filename, "w", encoding="utf-8") as file:
-        json.dump(my_obj, file, indent=4)
+        json.dump(my_obj, file)
