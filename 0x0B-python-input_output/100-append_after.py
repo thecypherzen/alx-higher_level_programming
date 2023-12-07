2ad36a755f2cf90e035cb0c15cec3d2fe9e20a6c
William Inyam <sw.inyam@outlook.com>
2023-12-07 19:22:53 +0000

    Task 13(100) update 1

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -14,9 +14,11 @@ Notes:
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """A function that inserts a line in a file
+    """A function that searches and updates a file.
 
-    The value is inserted after the line that contains a given string.
+    If ``search_string`` is found in the file(``filename``), then
+    ``new_string`` is inserted on a new line after the line where `
+        `search_string`` was found.
 
     Parameters:
         filename(:obj:str): the name of the file to read/write from/to
@@ -29,8 +31,6 @@ def append_after(filename="", search_string="", new_string=""):
         lines = file.readlines()
         for line in lines:
             if search_string in line:
-                if line[-1] != '\n':
-                    new_string = "\n" + new_string
                 lines.insert(lines.index(line) + 1, new_string)
         file.seek(0)
         file.truncate(0)
