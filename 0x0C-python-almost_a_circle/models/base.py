fadfd648ce84a3423e6fea8268c007d85b7fc88e
William Inyam <sw.inyam@outlook.com>
2023-12-11 22:39:17 +0000

    feat(base): add create method to base class
    Allows for the creation of an instance of the class
    that has all its values set.

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,11 +16,16 @@ class Base:
     Attributes:
         __nb_objects(int, private): number of class instances
         __init__(method, private): instance init method
+        create (method, class): creates instance of obj
+        from_json_string (method, static): creates a python list from
+            a json_string
+        save_to_file (method, class): saves python list to json string
+            in a file
         to_json_string(method, public): json_stringifyer of list
     """
     __nb_objects = 0
 
-    def __init__(self, id=None):
+    def __init__(self, id:any=None) -> None:
         """Initialises a class object
 
         If id is None, sets instance id to objects_count+1, else
@@ -36,6 +41,24 @@ class Base:
             self.id = Base.__nb_objects
         else:
             self.id = id
+
+    @classmethod
+    def create(cls, **dictionary:dict) -> object:
+        """creates an instance of a class
+
+        Args:
+            dictionary: keywords of args to set in instance of class
+
+        Returns: an instance of given class with all attributes set
+
+        Notes:
+            - must use the method def update(self, *args, **kwargs)
+            - **dictionary must be used as **kwargs of the method update
+            - use of `eval` not allowed
+        """
+        new_obj = cls(9, 10)
+        new_obj.update(**dictionary)
+        return new_obj
 
     @staticmethod
     def from_json_string(json_string:json) -> list:
@@ -55,27 +78,9 @@ class Base:
             return []
         return json.loads(json_string)
 
-
-    @staticmethod
-    def to_json_string(list_dictionaries:list) -> json:
-        """creates json_string representation of Square
-
-        Args:
-            list_dictionaries: list of dictionaries to
-                stringify
-
-        Returns: json_string form of `list_dictionaries`.
-        """
-        if not list_dictionaries or \
-            type(list_dictionaries) is not list or \
-                not len(list_dictionaries):
-            return json.dumps([])
-        return json.dumps(list_dictionaries)
-
-
     @classmethod
     def save_to_file(cls, list_objs:list) -> None:
-        """saves a jsonifyed list to a given file
+        """saves a jsonified list to a given file
 
         It writes the JSON string reps of `list_objs`
             to a file:
@@ -106,3 +111,19 @@ class Base:
         f_str = cls.to_json_string(f_obj)
         with open(f"{cls.__name__}.json", "w") as f:
             f.write(f_str)
+
+    @staticmethod
+    def to_json_string(list_dictionaries:list) -> json:
+        """creates json_string representation of Square
+
+        Args:
+            list_dictionaries: list of dictionaries to
+                stringify
+
+        Returns: json_string form of `list_dictionaries`.
+        """
+        if not list_dictionaries or \
+            type(list_dictionaries) is not list or \
+                not len(list_dictionaries):
+            return json.dumps([])
+        return json.dumps(list_dictionaries)
