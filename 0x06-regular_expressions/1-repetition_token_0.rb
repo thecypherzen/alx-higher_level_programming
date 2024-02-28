b442bc4851838069860aba587edad5343550e973
William Inyam <sw.inyam@outlook.com>
2024-02-28 11:31:50 +0000

    build(alx_devops): add task 1 file

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 puts ARGV[0].scan(/hbt{2,5}n/).join
 =======
 puts ARGV[0].scan(/hbt{2,}n/).join
@@ -8,3 +9,6 @@ puts ARGV[0].scan(/hbt{2,}n/).join
 =======
 puts ARGV[0].scan(/hbt{2,5}n/).join
 >>>>>>> 26d845e... fix(alx_devops): fix bug in task1 file
+=======
+puts ARGV[0].scan(/hbt{2,}n/).join
+>>>>>>> 1b86b77... build(alx_devops): add task 1 file
