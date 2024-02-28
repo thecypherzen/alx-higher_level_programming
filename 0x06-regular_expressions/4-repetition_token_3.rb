52d5dc2e63745be984d4b0d65686562727ec9027
William Inyam <sw.inyam@outlook.com>
2024-02-28 12:17:56 +0000

    fix(alx_devops): fix regex in task4 file

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 puts ARGV[0].scan(/hbt*+n/).join
 =======
 puts ARGV[0].scan(/hbt{0,4}n/).join
@@ -8,3 +9,6 @@ puts ARGV[0].scan(/hbt{0,4}n/).join
 =======
 puts ARGV[0].scan(/hbt*+n/).join
 >>>>>>> 2a063b2... fix(a_devops:0x06): fix regex in task4 file
+=======
+puts ARGV[0].scan(/hbt{0,4}n/).join
+>>>>>>> 38ef720... fix(alx_devops): fix regex in task4 file
