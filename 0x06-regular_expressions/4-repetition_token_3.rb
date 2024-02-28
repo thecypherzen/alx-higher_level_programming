46f41d6062a0ba52cfab9a7c424246c977c505a1
William Inyam <sw.inyam@outlook.com>
2024-02-28 12:20:03 +0000

    fix(a_devops:0x06): fix regex in task4 file

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -2,6 +2,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 puts ARGV[0].scan(/hbt*+n/).join
 =======
 puts ARGV[0].scan(/hbt{0,4}n/).join
@@ -12,3 +13,6 @@ puts ARGV[0].scan(/hbt*+n/).join
 =======
 puts ARGV[0].scan(/hbt{0,4}n/).join
 >>>>>>> 38ef720... fix(alx_devops): fix regex in task4 file
+=======
+puts ARGV[0].scan(/hbt*+n/).join
+>>>>>>> 2a063b2... fix(a_devops:0x06): fix regex in task4 file
