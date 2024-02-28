d24d718185ed95a14a6bb7f2041cf81882c901d7
William Inyam <sw.inyam@outlook.com>
2024-02-28 22:55:19 +0000

    build(a_devops:0x06): add task7 solution file v2-042024

--- 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -1,7 +1,29 @@
 #!/usr/bin/env ruby
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 puts ARGV[0].scan(/([A-Z]++)/).join
 =======
 puts ARGV[0].scan(/[A-Z]+/).join
 >>>>>>> 93ed88b... build(a_devops:0x06): add task7 solution file
+=======
+
+str = ARGV[0]
+regex = /(?>from:(?<sndr>\+?\w+))|(?>to:(?<rcvr>\+?\w+))|(flags:(?<flgs>(:?\-?[0-9])+))/
+matches = str.scan(regex)
+res = ""
+
+# if matches are found, then iterate over them to construct result
+if matches
+  matches.each do |match|
+    len = match.length - 1
+    for i in 0..len
+      if match[i]
+        res += match[i] if match[i]
+        res += ',' if i < (match.length - 1)
+      end
+    end
+  end
+end
+puts res
+>>>>>>> 3d3c7fd... build(a_devops:0x06): add task7 solution file
