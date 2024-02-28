3bd3287a35d22acd65fec209fb1d5e08b0de3c4d
William Inyam <sw.inyam@outlook.com>
2024-02-28 12:20:03 +0000

    fix(a_devops:0x06): fix regex in task4 file v2-042024

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -1,6 +1,10 @@
 #!/usr/bin/env ruby
 <<<<<<< HEAD
+<<<<<<< HEAD
 puts ARGV[0].scan(/hbt*+n/).join
 =======
 puts ARGV[0].scan(/hbt{0,4}n/).join
 >>>>>>> 38ef720... fix(alx_devops): fix regex in task4 file
+=======
+puts ARGV[0].scan(/hbt*+n/).join
+>>>>>>> 2a063b2... fix(a_devops:0x06): fix regex in task4 file
