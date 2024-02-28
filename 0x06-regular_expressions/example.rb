8f479c1122ac38c80e1467b0435627b55369d74c
William Inyam <sw.inyam@outlook.com>
2024-02-28 11:26:15 +0000

    build(alx_devops): add example.rb v2-042024

--- 0x06-regular_expressions/example.rb
+++ 0x06-regular_expressions/example.rb
@@ -1,5 +1,8 @@
 #!/usr/bin/env ruby
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> f3dc335... build(alx_devops): add example.rb
 #puts ARGV[0].scan(/(\w+).*\1/).join
 #mystr = "I code with java mostly, and javascript always. I know Typescript" +\
 #        "Other times, I use C++, C or Python"
@@ -14,6 +17,9 @@ if reg_scan
 else
   puts "no match"
 end
+<<<<<<< HEAD
 =======
 puts ARGV[0].scan(/127.0.0.[0-9]/).join
 >>>>>>> d1f75cf... fs(ased:root): restore deleted repos
+=======
+>>>>>>> f3dc335... build(alx_devops): add example.rb
