d06455e2db5731975bb751efca7d8ffe4127d4a9
William Inyam <sw.inyam@outlook.com>
2024-02-28 11:24:20 +0000

    build(alx_devops): add two new files v2-042024
    Example and regex note files

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,5 +1,9 @@
 #!/usr/bin/env ruby
 <<<<<<< HEAD
+<<<<<<< HEAD
 puts ARGV[0].scan(/(School)/).join
 =======
 >>>>>>> d1f75cf... fs(ased:root): restore deleted repos
+=======
+puts ARGV[0].scan(/(School)/).join
+>>>>>>> 7edfe4e... build(alx_devops): add two new files
