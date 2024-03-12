b042135eb2d569ff8320b43e8735e66bccc0df9d
William Inyam <sw.inyam@outlook.com>
2024-03-12 19:48:27 +0000

    build(ahlp:0x0D): add task 1 solution file

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# A script that creates the database hbtn_0c_0 in your MySQL server.
+# +Does so iff it doesn't exist already
+# +Use of `SHOW` or `SELECT` not allowed
+
+
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0
