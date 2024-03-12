900febee6507f5fbfd5b48e43847cffc0dfa4b55
William Inyam <sw.inyam@outlook.com>
2024-03-12 21:46:57 +0000

    fix(ahlp:0x0D): fix bugs in task 2 file
    The changes it makes include:
    	> remove execution permission from file
    	> change bash-style script to sql-style

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,6 +1,4 @@
-#!/usr/bin/env bash
-# A script deletes the database hbtn_0c_0 in your MySQL server.
-#+ iff it exists
-#+ use of `SHOW` or `SELECT` not allowed
+-- A script deletes the database hbtn_0c_0 in your MySQL server, iff it exists.
+-- The use of `SHOW` or `SELECT` not allowed
 
 DROP DATABASE IF EXISTS hbtn_0c_0;
