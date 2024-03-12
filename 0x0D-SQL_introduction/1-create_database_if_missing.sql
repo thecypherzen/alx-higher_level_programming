5d01fd43768d0e16a2d5fcf529c57d84732f9764
William Inyam <sw.inyam@outlook.com>
2024-03-12 21:39:47 +0000

    fix(ahlp:0x0D): fix bugs in file 1 solution file
    The changes include:
    	> convert bash script into an sql file
    	> remove execute permission on file

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,7 +1,5 @@
-#!/usr/bin/env bash
-# A script that creates the database hbtn_0c_0 in your MySQL server.
-# +Does so iff it doesn't exist already
-# +Use of `SHOW` or `SELECT` not allowed
-
+-- A script that creates the database hbtn_0c_0 in your MySQL server.
+-- +Does so iff it doesn't exist already
+-- Use of `SHOW` or `SELECT` not allowed
 
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0
