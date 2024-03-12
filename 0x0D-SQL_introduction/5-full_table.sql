23ae6b6345e41953b365b9a80b82858f0311a0eb
William Inyam <sw.inyam@outlook.com>
2024-03-12 22:17:08 +0000

    build(ahp:0x0D): add task 5 solution file

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,5 @@
+-- A script that prints the full description of the table first_table from the
+-- database hbtn_0c_0 in your MySQL server.
+-- Not allowed to use the `DESCRIBE` or `EXPLAIN` statements
+
+SHOW COLUMNS FROM hbtn_0c_0
