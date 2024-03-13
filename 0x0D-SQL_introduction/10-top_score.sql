66a9f358f159c54b06a7115cbedcb164a6dc3f87
William Inyam <sw.inyam@outlook.com>
2024-03-13 00:17:34 +0000

    build(ahp:0x0D): add task 10 solution file

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- A script that lists all records of the table second_table of the database.
+-- Results should display both the score and the name in the order score, name.
+-- Records should be ordered by score (top first)
+
+SELECT score, name FROM second_table ORDER BY score DESC;
