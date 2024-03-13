7b3ed09b052132193a94392da216ba4e01bf1268
William Inyam <sw.inyam@outlook.com>
2024-03-13 04:46:50 +0000

    build(ahp:0x0D): add task 14 solution file

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,5 @@
+-- A script that computes the score average of all records in the table
+-- + second_table of the database
+-- The result column name should be average
+
+SELECT AVG(score) AS "average" FROM second_table;
