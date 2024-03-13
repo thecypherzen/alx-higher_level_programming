70e5694ba382d422012ef1f3b96d72a9e99ebc9d
William Inyam <sw.inyam@outlook.com>
2024-03-13 00:08:22 +0000

    build(ahp:0x0D): add task 9 solution file

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,25 @@
+-- A script that creates the table second_table in the database
+-- + and inserts multiple rows into it.
+-- If second_table alreay exists, code should not fail
+-- Records for each row are provided on the intranet as seen below
+
+CREATE TABLE IF NOT EXISTS second_table
+(
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
+
+-- Insert records
+INSERT INTO
+	   second_table(id, name, score)
+	   VALUES(1, 'John', 10);
+INSERT INTO
+	   second_table(id, name, score)
+	   VALUES(2, 'Alex', 3);
+INSERT INTO
+	   second_table(id, name, score)
+	   VALUES(3, 'Bob', 14);
+INSERT INTO
+	   second_table(id, name, score)
+	   VALUES(4, 'George', 8);
