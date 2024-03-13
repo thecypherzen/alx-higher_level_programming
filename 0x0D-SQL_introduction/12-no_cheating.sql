450594f93f51dd89a73a4c6b0a12a4da0b28b651
William Inyam <sw.inyam@outlook.com>
2024-03-13 04:09:48 +0000

    build(ahp:0x0D): add task 12 solution file

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,6 @@
+-- A script that the score of Bob to 10 in the table second_table
+-- Allowed to use only the "name" field and nothing else
+
+UPDATE second_table
+	   SET score=10
+	   WHERE name='Bob'
