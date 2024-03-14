b134ceb5796d8c802181b786b0e3b9357e67b88f
William Inyam <sw.inyam@outlook.com>
2024-03-14 02:30:25 +0000

    build(ahp:0x0E): add task 1 solution file

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- A script that creates the MySQL server user user_0d_1
+
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'User_0d_1_pwd' IF NOT EXISTS;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
