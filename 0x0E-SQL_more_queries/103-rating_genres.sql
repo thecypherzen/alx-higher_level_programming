b16798fa9c6e424b317dd1ec5d7cf0a32d56b615
William Inyam <sw.inyam@outlook.com>
2024-03-14 22:51:54 +0000

    fix(ahp:0x0E): change column label in task 20(103) file

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -7,7 +7,7 @@
 -- Results must be sorted in descending order by their rating
 -- Can only use one SELECT statement
 
-SELECT tv_genres.name, SUM(tv_show_ratings.rate)
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS 'rating'
 FROM tv_genres
 	 JOIN tv_show_genres
 	 	  ON tv_genres.id = tv_show_genres.genre_id
