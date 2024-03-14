9fe708dab53b2f8df1fcdca17d7fa92425c6d6fe
William Inyam <sw.inyam@outlook.com>
2024-03-14 18:06:55 +0000

    fix(0x0E): fix task 17(100) to match requirements

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -9,11 +9,19 @@
 -- Results must be sorted in ascending order by the genre name
 -- Can only use a maximum of two SELECT statement
 
-SELECT DISTINCT tv_genres.name
+SELECT DISTINCT  tv_genres.name
 FROM tv_genres
 	 LEFT JOIN tv_show_genres
 	 	 ON tv_genres.id = tv_show_genres.genre_id
 	 LEFT JOIN tv_shows
 	 	 ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_shows.title != 'Dexter'
+WHERE tv_shows.title != 'Dexter' &&
+	  tv_genres.id NOT IN (SELECT tv_show_genres.genre_id
+	  			   FROM tv_shows
+				   		JOIN tv_show_genres
+				   			 ON tv_shows.id = tv_show_genres.show_id
+						JOIN tv_genres
+							 ON tv_genres.id = tv_show_genres.genre_id
+					WHERE tv_shows.title = 'Dexter'
+					)
 ORDER BY 1;
