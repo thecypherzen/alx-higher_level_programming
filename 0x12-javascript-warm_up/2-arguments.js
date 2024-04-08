e9b94aec8fb944e6a3126e83db05dcd7c64a9b6d
William Inyam <sw.inyam@outlook.com>
2024-04-08 15:59:54 +0000

    build(ahp:0x12): add task 2 file solution

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/*  prints  statements  based  on  arguments  passed  to  script  */
+
+const { argv } = require('node:process');
+
+if (argv.length < 3) {
+  console.log('No  argument');
+} else if (argv.length === 3) {
+  console.log('Argument  found');
+} else {
+  console.log('Arguments  found');
+}
