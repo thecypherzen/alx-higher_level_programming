65d21c6ccd7daa70d318697ffb57efc73361645f
William Inyam <sw.inyam@outlook.com>
2024-04-08 16:06:40 +0000

    fix(ahp:0x12): remove excess spaces

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,9 +4,9 @@
 const { argv } = require('node:process');
 
 if (argv.length < 3) {
-  console.log('No  argument');
+  console.log('No argument');
 } else if (argv.length === 3) {
-  console.log('Argument  found');
+  console.log('Argument found');
 } else {
-  console.log('Arguments  found');
+  console.log('Arguments found');
 }
