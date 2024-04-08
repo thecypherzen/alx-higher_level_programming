fc965610bee495414edb388d583b33a0b97f60ee
William Inyam <sw.inyam@outlook.com>
2024-04-08 21:14:02 +0000

    build(ahp:0x12): add task 12 solution file

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+/**
+ * A script that updates an object's value
+ */
+
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);
