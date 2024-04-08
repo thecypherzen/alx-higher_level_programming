2ba63e08a77c9b2d33bc1c741c228a6acb6992df
William Inyam <sw.inyam@outlook.com>
2024-04-08 19:49:29 +0000

    build(ahp:0x12): add task 6 solution file

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+/**
+ *  A script that prints three sentences using an array
+ */
+
+const myArr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i;
+for (i = 0; i < 3; i++) {
+  console.log(myArr[i]);
+}
