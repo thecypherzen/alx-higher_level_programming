d51ddd0120b618d00a737ae1dab0bb082874c3b6
William Inyam <sw.inyam@outlook.com>
2024-04-09 09:00:57 +0000

    fix(ahp:0x12): change use of arrow func

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -12,10 +12,11 @@ const myObject = {
 };
 console.log(myObject);
 
-myObject.incr = () => {
+myObject.incr = function(){
   this.value += 1;
 };
-
+console.log(Object.getOwnPropertyNames(myObject.__proto__));
+console.log(myObject);
 myObject.incr();
 console.log(myObject);
 myObject.incr();
