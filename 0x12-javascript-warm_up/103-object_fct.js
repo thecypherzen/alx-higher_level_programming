356eab599a36b9db6075fcc1e16b045996400c4c
William Inyam <sw.inyam@outlook.com>
2024-04-09 00:21:16 +0000

    build(ahp:0x12): add task 17(103) solution file

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+/**
+ * This script is only to be updated.
+ * The update is to add the function incr to increment the integer
+ * The use of 'var' is prohibited
+ */
+
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+myObject.incr = () => {
+  this.value += 1;
+};
+
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
