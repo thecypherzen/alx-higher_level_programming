ccae7bde749e7d4b3009617724bd2b5aad7bb04a
William Inyam <sw.inyam@outlook.com>
2024-04-15 22:55:09 +0000

    build(ahp:0x0F): add task 0 solution file

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" A script that lists all states in a Database """
+
+import MySQLdb as msdb
+import sys
+
+conn = msdb.connect(
+    host='localhost',
+    port=3306,
+    user=sys.argv[1],
+    passwd=sys.argv[2],
+    db=sys.argv[3]
+)
+
+cursor = conn.cursor()
+cursor.execute("SELECT * FROM states ORDER BY states.id")
+
+res = cursor.fetchall()
+for row in res:
+    print(row)
+
+cursor.close()
+conn.close()
