3d475f985afd035b6bb21b900fa5e9590f931cb9
William Inyam <sw.inyam@outlook.com>
2024-04-15 23:19:20 +0000

    build(ahp:0x0F): add task 1 solution file

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+""" A script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa
+
+- takes 3 arguments: mysql username, mysql password and database name
+- uses the module MySQLdb (import MySQLdb)
+- connects to a MySQL server running on localhost at port 3306
+- resulst sorted in ascending order by states.id
+- results displayed in the form:
+     (4, 'New York')
+     (5, 'Nevada')
+- code should not be executed when imported
+"""
+
+if __name__ == "__main__":
+    import MySQLdb as msdb
+    import sys
+
+    conn = msdb.connect(
+        host='localhost',
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cursor = conn.cursor()
+    query = """
+    SELECT id, name
+        FROM states
+    WHERE name like 'N%'
+    ORDER BY states.id
+    """
+    cursor.execute(query)
+
+    res = cursor.fetchall()
+    for row in res:
+        print(row)
+
+    cursor.close()
+    conn.close()
