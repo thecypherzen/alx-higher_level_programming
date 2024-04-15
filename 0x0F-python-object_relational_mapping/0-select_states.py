9d0904df5827e37e3042c9cc357bbcd546b0e52e
William Inyam <sw.inyam@outlook.com>
2024-04-15 23:04:26 +0000

    fix(ahp:0x0F): avoid execution on import

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
+
 """ A script that lists all states in a Database """
 
-import MySQLdb as msdb
-import sys
+if __name__ == "__main__":
+    import MySQLdb as msdb
+    import sys
 
-conn = msdb.connect(
-    host='localhost',
-    port=3306,
-    user=sys.argv[1],
-    passwd=sys.argv[2],
-    db=sys.argv[3]
-)
+    conn = msdb.connect(
+        host='localhost',
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
 
-cursor = conn.cursor()
-cursor.execute("SELECT * FROM states ORDER BY states.id")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY states.id")
 
-res = cursor.fetchall()
-for row in res:
-    print(row)
+    res = cursor.fetchall()
+    for row in res:
+        print(row)
 
-cursor.close()
-conn.close()
+    cursor.close()
+    conn.close()
