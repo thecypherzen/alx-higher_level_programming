fe4b306f831818d6990bcad7188959d1a3a4ff78
William Inyam <sw.inyam@outlook.com>
2024-04-16 08:19:00 +0000

    build(ahp:0x0F): add task 2 solution

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+""" A script that that takes in an argument and displays all values in the
+states table of hbtn_0e_0_usa where name matches the argument.
+
+- takes 4 arguments: mysql username, mysql password,  database name,
+    and state name searched.
+- uses the module MySQLdb (import MySQLdb)
+- connects to a MySQL server running on localhost at port 3306
+- resulst sorted in ascending order by states.id
+- results displayed in the form:
+     (4, 'New York')
+- code should not be executed when imported
+"""
+
+if __name__ == "__main__":
+    import MySQLdb as msdb
+    import sys
+
+    conn = msdb.connect(
+        host='localhost',
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cursor = conn.cursor()
+    query = """
+    SELECT id, name
+        FROM states
+    WHERE name = %s
+    ORDER BY states.id
+    """
+    cursor.execute(query, (sys.argv[4],))
+
+    res = cursor.fetchall()
+    for row in res:
+        print(row)
+
+    cursor.close()
+    conn.close()
