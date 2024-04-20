2ba3290cc30918fa3bef2683c25a5fb43e237860
William Inyam <sw.inyam@outlook.com>
2024-04-20 20:03:42 +0000

    build(asd:0x0A): add task 1 solution

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,14 @@
 # installs flask on machine using pip3
+<<<<<<< HEAD
 # first uninstalls all dependencies if they exist already
+=======
+>>>>>>> a32828b... build(asd:0x0A): add task 1 solution
 
 package{'python3':
   ensure  => installed,
 }
 
+<<<<<<< HEAD
 $packages = ['jinja2','werkzeug','itsdangerous','importlib-metadata','click', 'flask']
 each($packages) | $pkg | {
   exec{"pip3_remove ${pkg}":
@@ -23,3 +27,22 @@ exec{'install_flask':
   command => 'pip3 install flask==2.1.0 werkzeug==2.1.1',
   path    => ['/usr/bin', '/bin'],
 }
+=======
+package{'pip3':
+  ensure => installed,
+}
+
+exec{'install_flask':
+  command => 'pip3 install flask==2.1.0',
+  path    => ['/usr/bin', '/bin'],
+  unless  => 'pip3 show flask',
+}
+
+
+#exec{'test':
+#  command   => 'echo my_test_command',
+#  path      => ['/usr/bin/', '/bin'],
+#  onlyif    => 'ls . | grep "README.md" | wc -l',
+#  logoutput => true,
+#}
+>>>>>>> a32828b... build(asd:0x0A): add task 1 solution
