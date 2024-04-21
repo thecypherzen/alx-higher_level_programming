dbece7c9645bf252a048eb525b6f8e4a11c38e41
William Inyam <sw.inyam@outlook.com>
2024-04-21 20:24:57 +0000

    fix(asd:0x0A): resolve task 1 import err

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,6 +2,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 # first uninstalls all dependencies if they exist already
 =======
 >>>>>>> a32828b... build(asd:0x0A): add task 1 solution
@@ -10,11 +11,15 @@
 >>>>>>> 014f41f... fix(asd:0x0A): resolve task 1 import err
 =======
 >>>>>>> a32828b... build(asd:0x0A): add task 1 solution
+=======
+# first uninstalls all dependencies if they exist already
+>>>>>>> 014f41f... fix(asd:0x0A): resolve task 1 import err
 
 package{'python3':
   ensure  => installed,
 }
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -95,3 +100,23 @@ exec{'install_flask':
 >>>>>>> 8c05ea9... style(asd:0x0A): pylintyfy task 1 file
 =======
 >>>>>>> a32828b... build(asd:0x0A): add task 1 solution
+=======
+$packages = ['jinja2','werkzeug','itsdangerous','importlib-metadata','click', 'flask']
+each($packages) | $pkg | {
+   exec{"pip3_remove $pkg":
+	  command => "pip3 uninstall --yes $pkg",
+	  path    => ['/bin/usr', '/bin'],
+	  onlyif  => "pip3 show $pkg",
+   }
+   exec{"apt_remove $pkg":
+      command => "apt-get remove python3-$pkg",
+	  path    => ['/bin/usr', '/bin'],
+	  onlyif  => "pip3 show $pkg",
+   }
+}
+
+exec{'install_flask':
+   command => 'pip3 install flask==2.1.0 werkzeug==2.1.1',
+   path    => ['/usr/bin', '/bin'],
+}
+>>>>>>> 014f41f... fix(asd:0x0A): resolve task 1 import err
