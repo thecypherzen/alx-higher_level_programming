86190b4685e7974c511063e3b2b4cf139e315d5c
William Inyam <sw.inyam@outlook.com>
2024-04-25 22:57:41 +0000

    Revert "refactor(ahp:0x10): make script 0 3 lines"
    This reverts commit 9e05451910d121ebd25244ef3ab54a7947fbe929.

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 # script that takes in a URL, sends a request to that URL, and displays the
-echo "$(curl -Is "$1" | grep Content-Length | tr -d ' ' | cut -d : -f2)"
+# +size of the body of the response in bytes using curl
+
+if (( "$#" > 0 )); then
+    res="$(curl -Is "$1" | grep Content-Length | tr -d ' ' | cut -d : -f2)"
+    echo "$res"
+fi
