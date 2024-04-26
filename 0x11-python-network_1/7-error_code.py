871f5457a32a57952ac9cfafa5c177dffdaa6217
William Inyam <sw.inyam@outlook.com>
2024-04-26 19:31:02 +0000

    build(ahp:0x11): add task 7 solution

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""A script that takes in a URL, sends a request to the URL and displays the
+   body of the response.
+
+   => If the HTTP status code is greater than or equal to 400, print:
+      Error code: followed by the HTTP status code
+   => must use the packages 'urllib' and 'sys' and can only import them
+   => no need to check arguments passed to the script (number or type)
+"""
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    url = sys.argv[1]
+    response = requests.get(url).__dict__
+    status_code = response.get("status_code", 999)
+    if status_code >= 400:
+        print("Error code: ", status_code)
+    else:
+        print(str(response.get("_content"), encoding="ascii"))
