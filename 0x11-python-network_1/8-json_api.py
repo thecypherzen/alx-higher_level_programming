52a80fedd7c1d0ad7f57a5740d4740c3aad1fd5b
William Inyam <sw.inyam@outlook.com>
2024-04-27 03:59:50 +0000

    build(ahp:0x11): add task 8 solution

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""A script that takes takes in a letter and sends a POST request to
+   http://0.0.0.0:5000/search_user with the letter as a parameter.
+
+   => the letter must be sent in the variable q
+   => if no argument is given, set q=""
+   => if the response body is properly JSON formatted and not empty, display
+      the id and name like this: [<id>] <name>, otherwise:
+      => display 'Not a valid JSON' if the JSON is invalid
+      => display 'No result' if the JSON is empty
+   => must use the packages 'requests' and 'sys' and only alled to import them
+"""
+if __name__ == "__main__":
+    import requests
+    from requests.exceptions import JSONDecodeError as jsonError
+    import sys
+
+    if len(sys.argv) < 2:
+        letter = ""
+    else:
+        letter = sys.argv[1]
+
+    data = {"q": letter}
+    url = "http://0.0.0.0:5000/search_user"
+    response = requests.post(url, data=data)
+    try:
+        json_res = response.json()
+        if not json_res:
+            print("No result")
+        else:
+            print(json_res)
+            print(f"[{json_res.get('id')}] {json_res.get('name')}")
+    except jsonError:
+        print("Not a valid JSON")
