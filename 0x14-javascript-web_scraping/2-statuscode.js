325f1b2320be177084046af2ed1b1fd2b6f1a9bc
William Inyam <sw.inyam@outlook.com>
2024-05-22 03:46:33 +0000

    build(ahp:0x14:2): add task solution

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+/** displays the status of a get request
+ *  - the first arg is the url to the resource
+ *  - the status code must be printed like this: code: <status code>
+ *  - must use the module request
+ */
+
+const request = require('request');
+const { argv } = require('node:process');
+const url = argv[2];
+
+request(url, (err, res, body) => {
+  if (!err) {
+    console.log(res.statusCode);
+  }
+});
