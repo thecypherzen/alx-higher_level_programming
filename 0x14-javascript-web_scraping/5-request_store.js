9a2001de7a493a2196547a552c48a4517763d030
William Inyam <sw.inyam@outlook.com>
2024-05-22 06:20:02 +0000

    build(ahp:0x14:5): add task solution

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+/** gets the contents of a webpage and stores it in a file.
+ *  - the first argument is the URL to request
+ *  - the second argument the file path to store the body response
+ *  - the file must be UTF-8 encoded
+ *  - must use the module 'request'
+ */
+
+const { argv } = require('node:process');
+const fs = require('fs');
+const request = require('request');
+
+const url = `${argv[2]}`;
+const path = `${argv[3]}`;
+
+request(url, { json: true }, (err, res, body) => {
+  if (!err) {
+    fs.writeFile(path, body, (error) => {
+      if (error) {
+        console.log(error);
+      }
+    });
+  }
+});
