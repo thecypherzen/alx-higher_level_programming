cf912c2060480424bbaf49e7c80bb8617c207ea7
William Inyam <sw.inyam@outlook.com>
2024-05-23 13:56:14 +0000

    refactor(ahp:0x14:4): use regex and for..of loop

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-/** prints the title number of movies where the character “Wedge Antilles”
+/** prints the number of movies where the character “Wedge Antilles”
  *  is present.
  *  - The first argument is the API URL of the Star wars API:
- *     'https://swapi-api.alx-tools.com/api/films/'
+ *     'https://swapi-api.alx-tools.com/api/films'
  *  - Wedge Antilles is character ID 18 - your script must use this ID
  *     for filtering the result of the API
  *  - must use the module request
@@ -17,12 +17,24 @@ request(url, { json: true }, (err, res, body) => {
     console.log(err);
   } else {
     if (res.statusCode === 200) {
-      const wedgeUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
-      let results = res?.body?.results;
-      results = results.filter(
+      const regex = /[*]*18/;
+      const results = body?.results;
+      let count = 0;
+      // console.log(results);
+      for (const result of results) {
+        for (const actor of result.characters) {
+          if (regex.test(actor)) {
+            count += 1;
+          }
+        }
+      }
+      console.log(count);
+      /*
+      results = newResults.filter(
         (film) => film.characters.includes(wedgeUrl)
       );
-      console.log(results.length);
+        console.log(results.length);
+      */
     }
   }
 });
--- 0x14-javascript-web_scraping/test.js
+++ 0x14-javascript-web_scraping/test.js
@@ -1,15 +1,5 @@
 #!/usr/bin/node
 
-/*
-const numbers = [0, 1, 2, 3, 4, 5, 6];
-console.log(numbers.reduce(
-    (a, b) => { console.log(a, b);
-		   return a + b ;
-		 }, 0
-));
-*/
-myobj = {};
-console.log(myobj['1'] || 0);
-myobj['1'] = 5;
-console.log(myobj['1'] || 0);
-console.log(parseInt(true));
+let est = /[*]*18/;
+let str = 'https://swapi-api.alx-tools.com/api/people/18/';
+console.log(est.test(str));
