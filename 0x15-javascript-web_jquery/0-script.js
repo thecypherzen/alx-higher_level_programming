05aeccd41546df34017deeb8722450ecbb844b17
William Inyam <sw.inyam@outlook.com>
2024-05-29 17:53:39 +0000

    build(ahp:0x15): add task 1 solution

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,3 +1,4 @@
 #!/usr/bin/node
+// Changes the color of first header tag to red usin valina JS
 
 document.querySelector('header').style.color = '#FF0000';
---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// Changes color of first header to red using JQuery
+
+$("document").ready(() => {
+    $("header").css("color", "#FF0000");
+});
