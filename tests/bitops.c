537c04aa6183e5f2dcbba43e6c0efc788c2e3d75
William INyam <sw.inyam@outlook.com>
2024-08-08 14:53:11 +0000

    cleanup(ahp:) rename tests folder

--- tests/bitops.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-	int n = 6;
-	printf("%d\n", (~n + 1) & n);
-}
--- tests/shell.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-var=$(echo this | rev | cut -d "." -f1 | rev)
-if [[ var -eq "py" ]]; then
-	echo "yes"
-else
-	echo "no"
-fi
